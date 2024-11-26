6fccdb2826bfad464f0c455a46ca80470c1b1e1f
rare-wolf <rarewolf626@gmail.com>
2024-11-26 08:11:20 -0800

    rebuild portfolio

---
+++ precache-manifest.d031b4ea2e3d12d1bac412a568a9d8b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1967178ce072ae18f8a9eaa0b5feb8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c735299006fb1824719",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ff7704af89c8175ecc4a",
+    "url": "/static/css/main.d10d0330.chunk.css"
+  },
+  {
+    "revision": "2c735299006fb1824719",
+    "url": "/static/js/2.bf3a0746.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf3a0746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7704af89c8175ecc4a",
+    "url": "/static/js/main.ce7a25d0.chunk.js"
+  },
+  {
+    "revision": "eae82fab02ea6cf41c01",
+    "url": "/static/js/runtime-main.43615285.js"
+  },
+  {
+    "revision": "7bcb31312f3ed2b51e536dd3f625c60d",
+    "url": "/static/media/Resume.7bcb3131.pdf"
+  },
+  {
+    "revision": "5d87add32e53e7171ee63f05f342e677",
+    "url": "/static/media/coding.5d87add3.jpg"
+  },
+  {
+    "revision": "b657f98153685890bd03ba6de7f84d25",
+    "url": "/static/media/curved.b657f981.png"
+  }
+]);
